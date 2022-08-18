1e04b8c5606e7e2e848cdae6e0cf626e04d4bba0
xeozz12 <111479995+xeozz12@users.noreply.github.com>
2022-08-17 23:58:31 -0700

    Add files via upload

---
+++ phyton2-ryan.py
@@ -0,0 +1,8 @@
+nama_depan="ryan"
+nama_tengah="-"
+nama_belakang="andrian"
+nama_lengkap="ryan andrian"
+print("nama depan anda adalah",nama_depan)
+print("nama tengah saya adalah",nama_tengah)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)
