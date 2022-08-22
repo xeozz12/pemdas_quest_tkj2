a2af35d2668e8752188d71ff53695418b25df4e6
xeozz12 <111479995+xeozz12@users.noreply.github.com>
2022-08-22 12:11:32 +0700

    Add files via upload

---
+++ python latihan.py
@@ -0,0 +1,15 @@
+print("halo semuanya")
+print("perkenalkan")
+nama_saya_adalah=input("nama saya adalah")
+nama_lengkap=input("nama lengkap saya adalah")
+a = int(input("masukkan nilai A: "))
+b = float(input("masukkan nilai  B"))
+c = a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukkan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius =" ,x, "fahrenheit")
